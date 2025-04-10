d6700e09941d1e4ae58e90769364dd56db565525
21Neil <88711171+21Neil@users.noreply.github.com>
2025-04-10 17:00:30 +0800

    feat: add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import './App.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar />
+    </>
   )
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router';
+import styles from './Navbar.module.css';
+import { ShoppingBag } from 'lucide-react';
+
+const Navbar = () => {
+  return (
+    <header className={styles.header}>
+      <Link to={'/'}>
+        <h1 className={styles.header__title}>Neil Shop</h1>
+      </Link>
+      <nav className={styles.header__navbar}>
+        <ul className={styles.navbar__list}>
+          <li>
+            <Link to={'/'} className={styles.navbar__item}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.navbar__item}>Shop</Link>
+          </li>
+          <li>
+            <Link className={styles.navbar__item}>
+              <ShoppingBag color='black' size={28} strokeWidth={1.5} />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
